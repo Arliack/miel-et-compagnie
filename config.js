7626e120eb47815463e6d8da9645a194c315ad1b
arliack <jpagerit@gmail.com>
2026-08-30 15:03:51 +0200

    Pointe vers le déploiement Apps Script fonctionnel

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // URL du web app Google Apps Script (à mettre à jour après chaque nouveau déploiement "clasp deploy").
-const API_URL = "https://script.google.com/macros/s/AKfycbzuSgPq4SKqTXCrmoHTAGqH23L2FrrmAjkx2KoOHYjgukCOvgYSf1yCk_hPn8sPiyCFJQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyt0B5uTLwip0HP7vOAfToWIdDd6ksI3k1jj7B92aK4h2qADmh85T2WpEYrd-en1tH07Q/exec";
